437d6df76016c8a029deb23efbaf9f22222791f0
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-24 21:36:29 -0500

    commit tree 8f0a34900d0fe287771e68a659bf02f742948d66

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  state = {
+    text: ''
+  }
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addRestaurant(this.state.text)
+    this.setState({text: ''})
+  }
+
   render() {
     return (
       <div>
         Restaurant Input
+        <form onSubmit={ (event) => this.handleSubmit(event)}>
+          <label>Restaurant:</label>
+          <input
+            placeholder='Enter Restaurant Name'
+            type="text"
+            value={this.state.text}
+            onChange={(event) => this.handleOnChange(event)} />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
-};
 
+};
 export default RestaurantInput;
--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
+  renderRestaurants = () => {
+    return this.props.restaurants.map(restaurant => {
+      return (
+        <Restaurant
+          deleteRestaurant={this.props.deleteRestaurant}
+          key={restaurant.id}
+          restaurant={restaurant}
+        />
+      )
+    })
+  }
+
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.renderRestaurants()}
       </ul>
     );
   }
 };
 
-export default Restaurants;
+export default Restaurants; 
